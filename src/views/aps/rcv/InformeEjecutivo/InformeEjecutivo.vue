8a4fee01a9b0a02ed7c50c9d3f71369fd026c8fb
Jesus Narvaez Diaz <jesusDiaz@gmail.com>
2020-09-22 12:26:57 -0500

    agregado de ultima fila en negrita en informe rcv, cambio de fuente en simple-table, se elimino el calculo del promedio de tiempo de encuesta en frontend, se soluciono el problema de no poder mandar los parametros nulos y decarga de PDF de rcv y covid

--- src/views/aps/rcv/InformeEjecutivo/InformeEjecutivo.vue
+++ src/views/aps/rcv/InformeEjecutivo/InformeEjecutivo.vue
@@ -20,7 +20,7 @@
             >
             </c-date>
           </v-col>
-          <v-col cols="12" sm="12" md="4" lg="4">
+          <v-col cols="12" sm="12" md="3" lg="3">
             <c-select-complete
                 v-model="data.departamentos"
                 label="Departamentos"
@@ -35,6 +35,16 @@
             <v-btn color="green" class="white--text" @click="getDataInforme" block>
               <span>Filtrar</span>
             </v-btn>
+          </v-col>
+          <v-col class="ml-0 pl-0" cols="12" sm="12" md="1" lg="1">
+            <v-tooltip top>
+              <template v-slot:activator="{ on }">
+                <v-btn color="red" icon v-on="on" :disabled="loadingPDF" :loading="loadingPDF" @click="descargarPDF">
+                  <v-icon>fas fa-file-pdf</v-icon>
+                </v-btn>
+              </template>
+              <span>Generar PDF</span>
+            </v-tooltip>
           </v-col>
         </v-row>
       </v-card-title>
@@ -51,6 +61,8 @@
                 <simple-table
                     :titulo="'Por Riesgo OMS'"
                     :data="dataInforme.resumen_operativo"
+                    :lastRowBold="true"
+                    :alignNumbersRight="true"
                     :headers="['Usuario','Regional', 'Cantidad', 'Duración Promedio', 'Bajo', 'Medio', 'Alto', 'Indeterminado']"
                 ></simple-table>
               </v-card-text>
@@ -67,6 +79,7 @@
                     <simple-table
                         :data="dataInforme.riesgo_oms"
                         :headers="['Riesgo','Cantidad']"
+                        :alignNumbersRight="true"
                     ></simple-table>
                   </v-col>
                   <v-col cols="12" sm="12" md="4" lg="4">
@@ -76,6 +89,7 @@
                     <simple-table
                         :data="dataInforme.riesgo_findrisc"
                         :headers="['Riesgo','Cantidad']"
+                        :alignNumbersRight="true"
                     ></simple-table>
                   </v-col>
                   <v-col cols="12" sm="12" md="4" lg="4">
@@ -85,6 +99,7 @@
                     <simple-table
                         :data="dataInforme.riesgo_morisky"
                         :headers="['Riesgo','Cantidad']"
+                        :alignNumbersRight="true"
                     ></simple-table>
                   </v-col>
                 </v-row>
@@ -103,12 +118,14 @@
                         :titulo="'Encuestas de Riesgo Cardiovascular por Departamento'"
                         :data="dataInforme.por_departamento"
                         :headers="['Departamento','Cantidad']"
+                        :alignNumbersRight="true"
                     ></simple-table>
                   </v-col>
                   <v-col cols="12" sm="12" md="5" lg="5">
                     <simple-table
                         :data="dataInforme.por_departamento_regimen"
                         :headers="['Departamento', 'Régimen','Cantidad']"
+                        :alignNumbersRight="true"
                     ></simple-table>
                   </v-col>
                 </v-row>
@@ -124,7 +141,6 @@
 
 <script>
 import {mapGetters} from "vuex";
-
 const SimpleTable = () => import('../../../../components/SimpleTable/SimpleTable')
 export default {
   name: "InformeEjecutivo",
@@ -132,7 +148,8 @@ export default {
     dataInforme: [],
     data: {},
     loading: false,
-    defaultDepartamentos: []
+    defaultDepartamentos: [],
+    loadingPDF: false
   }),
   components: {
     SimpleTable
@@ -146,23 +163,13 @@ export default {
   methods: {
     getDataInforme() {
       this.loading = true
-      let data = this.setParametros()
-      this.axios.post('informe-ejecutivo_rcv', data).then(response => {
+      this.axios.post('informe-ejecutivo_rcv', this.data).then(response => {
         this.dataInforme = response.data
-        this.returnTotales()
         this.loading = false
       }).catch(error => {
         this.loading = false
         this.$store.commit('snackbar', {color: 'error', message: ` al conseguir datos de informe`, error: error})
       })
-    },
-    setParametros() {
-      this.data = {
-        fecha_inicio: this.data.fecha_inicio ? this.data.fecha_inicio : this.moment().format('YYYY-MM-DD'),
-        fecha_fin: this.data.fecha_fin ? this.data.fecha_fin : this.moment().format('YYYY-MM-DD'),
-        departamentos: this.data.departamentos && this.data.departamentos.length ? this.data.departamentos : this.returnDepartamentos()
-      }
-      return this.data
     },
     returnDepartamentos() {
       if (!this.defaultDepartamentos.length && (this.complementosRCV && this.complementosRCV.departamentos_rcv && this.complementosRCV.departamentos_rcv.length)) {
@@ -172,27 +179,33 @@ export default {
       }
       return this.defaultDepartamentos
     },
-    returnTotales() {
-      if (this.dataInforme.resumen_operativo.length) {
-        let total = {c1: "TOTAL", c2: "", c3: 0, c4: 0, c5: 0, c6: 0, c7: 0, c8: 0}
-        for (const [llave, valor] of Object.entries(this.dataInforme.resumen_operativo)) {
-          llave
-          total.c3 += valor.cantidad
-          total.c4 += parseFloat(valor.promedio)
-          total.c5 += valor.Bajo
-          total.c6 += valor.Medio
-          total.c7 += valor.Alto
-          total.c8 += valor.Indeterminado
+    descargarPDF(){
+      this.loadingPDF = true
+      this.axios( {
+        url: `generar-pdf-informe-ejecutivo_rcv?fecha_inicio=${this.data.fecha_inicio ? this.data.fecha_inicio : ''}&fecha_final=${this.data.fecha_fin ? this.data.fecha_fin : ''}&departamentos=${this.data.departamentos ? this.data.departamentos : []}`, //your url,
+        method: 'GET',
+        responseType: 'blob', // important
+      }).then(async response => {
+        if(response.status === 204) {
+          this.$store.commit('snackbar', {color: 'info', message: 'Los parametros aplicados no han generado registros para crear PDF'})
+        }else{
+          const fileURL = window.URL.createObjectURL(
+              new Blob([response.data], {type: 'application/pdf'}))
+          await window.open(fileURL, '_blank')
         }
-        this.dataInforme.resumen_operativo.push(total)
-        let promedioTotal = this.clone(this.dataInforme.resumen_operativo)
-        promedioTotal.pop()
-        this.dataInforme.resumen_operativo[this.dataInforme.resumen_operativo.length - 1].c4 = parseFloat(window.lodash.sumBy(promedioTotal, x => parseFloat(x.promedio)) / (this.dataInforme.resumen_operativo.length - 1)).toFixed(1)
-      }
-    }
+        this.loadingPDF = false
+      }).catch(error => {
+        this.loadingPDF = false
+        this.$store.commit('snackbar', {color: 'error', message: 'al descargar el PDF', error: error})
+      })
+    },
   },
   created() {
-    this.setParametros()
+    this.data = {
+      fecha_inicio: this.data.fecha_inicio ? this.data.fecha_inicio : this.moment().format('YYYY-MM-DD'),
+      fecha_fin: this.data.fecha_fin ? this.data.fecha_fin : this.moment().format('YYYY-MM-DD'),
+      departamentos: this.data.departamentos && this.data.departamentos.length ? this.data.departamentos : this.returnDepartamentos()
+    }
     this.getDataInforme()
   }
 }
