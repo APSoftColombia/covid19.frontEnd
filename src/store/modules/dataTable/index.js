7263877965b579d7c50e24f1a0975051351cfd7d
Heisenberg <xmyisus19@gmail.com>
2020-10-30 10:48:05 -0500

    Nuevas columnas creacion y actualizacion en tabla de encuestas rcv

--- src/store/modules/dataTable/index.js
+++ src/store/modules/dataTable/index.js
@@ -22,7 +22,7 @@ const state = {
         tablaMuestras: 0,
         tablaMuestrasVersion: 1,
         tablaEncuestasRCV: 0,
-        tablaEncuestasRCVVersion: 5,
+        tablaEncuestasRCVVersion: 6,
         tablaUsers: 0,
         tablaUsersVersion: 0,
         tablaCets: 0,
