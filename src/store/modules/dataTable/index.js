d86ee1e84158d9f99515edb2058b763d814a4622
gerson castillo hernandez <gersoten@outlook.com>
2020-11-03 17:47:47 -0500

    demanda covid por defecto y solucion de problemas del conteo de no efectivas

--- src/store/modules/dataTable/index.js
+++ src/store/modules/dataTable/index.js
@@ -28,7 +28,7 @@ const state = {
         tablaCets: 0,
         tablaCetsVersion: 0,
         tablaDemandaInducida: 0,
-        tablaDemandaInducidaVersion: 8
+        tablaDemandaInducidaVersion: 9
     }
 }
 
