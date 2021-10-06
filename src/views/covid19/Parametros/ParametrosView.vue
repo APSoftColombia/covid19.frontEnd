d31c0dea3c6a0ddbfb2232f2ee5222518c074ad6
Jesus Narvaez <susje999@hotmail.com>
2021-10-06 10:00:59 -0500

    añadido de condicionales para muestra de items en parametros

--- src/views/covid19/Parametros/ParametrosView.vue
+++ src/views/covid19/Parametros/ParametrosView.vue
@@ -6,8 +6,8 @@
                 <v-list two-line class="notification-wrap">
                     <v-list-item-group v-model="currentOption" color="indigo">
                         <template v-for="option in options">
-                            <v-hover v-slot:default="{ hover }" :key="`option-${ option.value }`">
-                                <v-list-item @click="selectTable(option.value)" v-if="option.value === 2 ? datosEmpresa.departamento_id === 29 : true">
+                            <v-hover v-if="permisos[option.permission]" v-slot:default="{ hover }" :key="`option-${ option.value }`">
+                                <v-list-item @click="selectTable(option.value)">
                                     <v-list-item-avatar class="my-1 align-self-center">{{ (option.value + 1) }}</v-list-item-avatar>
                                     <v-list-item-content class="pa-0">
                                         <v-list-item-title><h5 class="mb-0 text-truncate">{{ option.name }}</h5></v-list-item-title>
@@ -18,16 +18,16 @@
                     </v-list-item-group>
                 </v-list>
             </v-col>
-            <v-col cols="12" sm="12" md="8" lg="9" v-if="currentOptionC === 0">
+            <v-col cols="12" sm="12" md="8" lg="9" v-if="currentOptionC === 0 && permisos.barriosVeredasCrud">
                 <barrios-veredas/>
             </v-col>
-            <v-col cols="12" sm="12" md="8" lg="9" v-if="currentOptionC === 1">
+            <v-col cols="12" sm="12" md="8" lg="9" v-if="currentOptionC === 1 && permisos.laboratoriosCrud">
                 <laboratorios/>
             </v-col>
-            <v-col cols="12" sm="12" md="8" lg="9" v-if="currentOptionC === 2">
+            <v-col cols="12" sm="12" md="8" lg="9" v-if="currentOptionC === 2 && permisos.vacunadoresCrud">
               <Vacunadores/>
             </v-col>
-          <v-col cols="12" sm="12" md="8" lg="9" v-if="currentOptionC === 3">
+          <v-col cols="12" sm="12" md="8" lg="9" v-if="currentOptionC === 3 && permisos.dependenciasCrud">
               <Dependencias/>
             </v-col>
         </v-row>
@@ -35,8 +35,6 @@
 </template>
 
 <script>
-    import {mapGetters} from "vuex";
-
     const BarriosVeredas = () => import('./Components/BarriosVeredas')
     const Laboratorios = () => import('./Components/Laboratorios')
     const Vacunadores = () => import('./Components/Vacunadores')
@@ -52,24 +50,24 @@
         data: () => ({
             currentOption: 0,
             options: [
-                {name: 'Barrios y Veredas', value: 0},
-                {name: 'Laboratorios', value: 1},
-                {name: 'Vacunadores', value: 2},
-                {name: 'Dependencias', value: 3}
+                {name: 'Barrios y Veredas', value: 0, permission: 'barriosVeredasCrud'},
+                {name: 'Laboratorios', value: 1, permission: 'laboratoriosCrud'},
+                {name: 'Vacunadores', value: 2, permission: 'vacunadoresCrud'},
+                {name: 'Dependencias', value: 3, permission: 'dependenciasCrud'}
             ]
         }),
         computed: {
             currentOptionC(){
                 return this.currentOption
             },
-          ...mapGetters([
-                'datosEmpresa'
-            ])
+            permisos() {
+              return this.$store.getters.getPermissionModule("parametros");
+            },
         },
         methods: {
             selectTable(value){
                 this.currentOption = value
-            }
+            },
         }
     }
 </script>
