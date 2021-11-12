d4058a475d1af0c8fcffd45d315eeece22c2a859
Jesus Narvaez <susje999@hotmail.com>
2021-11-12 11:11:53 -0500

    muestra de dosis aplicadas en modulo de tamizajes y envio a modal de detalle de la dosis aplicada

---
+++ src/views/covid19/tamizaje/BotonVacunacion.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-tooltip top v-if="item && item.length">
+    <template v-slot:activator="{on}">
+      <v-btn
+          v-on="on"
+          :class="`${textColor}--text`"
+          @click="$emit('click', item)"
+          text
+          icon
+      >
+        <v-icon>mdi mdi-needle</v-icon>
+      </v-btn>
+    </template>
+    <span>Este registro tiene {{ item.length }} vacunas aplicadas</span>
+  </v-tooltip>
+</template>
+
+<script>
+export default {
+  name: "BotonVacunacion",
+  props: {
+    item: {
+      type: [Array, Object],
+      Default: null
+    },
+    textColor: {
+      type: String,
+      default: null
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
