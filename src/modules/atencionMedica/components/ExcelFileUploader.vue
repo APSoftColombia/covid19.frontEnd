0f013fb061c1075a2965972b03fa369c684bbd90
krlit <krlitozagusto@hotmail.es>
2022-02-14 06:54:03 -0500

    campo mes al cargarar archivo

--- src/modules/atencionMedica/components/ExcelFileUploader.vue
+++ src/modules/atencionMedica/components/ExcelFileUploader.vue
@@ -3,7 +3,7 @@
       v-model="dialog"
       scrollable
       persistent
-      max-width="900px"
+      max-width="1020px"
   >
     <template v-slot:activator="{ on, attrs }">
       <v-btn
@@ -63,34 +63,54 @@
           <v-row dense>
             <v-col cols="12">
               <ValidationObserver ref="formFile">
-                <ValidationProvider
-                    name="archivo" rules="required" v-slot="{ errors }">
-                  <v-file-input
-                      label="Archivo"
-                      v-model="file"
-                      accept=".xls,.xlsx"
-                      prepend-icon="mdi-file-upload"
-                      :error-messages="errors"
-                      outlined
-                  >
-                    <template v-slot:append-outer>
-                      <v-btn
-                          v-if="file"
-                          style="bottom: 8px !important;"
-                          color="primary"
-                          dark
-                          @click="fileUpload"
+                <v-row>
+                  <v-col cols="12" sm="4" xl="3">
+                    <c-date
+                        v-model="month"
+                        label="Mes de cargue"
+                        type="month"
+                        name="mes de cargue"
+                        rules="required"
+                        :max="moment().format('YYYY-MM-DD')"
+                        :dense="false"
+                    />
+                  </v-col>
+                  <v-col cols="12" sm="8" xl="9">
+                    <ValidationProvider
+                        name="archivo"
+                        rules="required"
+                        v-slot="{ errors }"
+                    >
+                      <v-file-input
+                          label="Archivo"
+                          v-model="file"
+                          accept=".xls,.xlsx"
+                          prepend-icon="mdi-file-upload"
+                          :error-messages="errors"
+                          outlined
                       >
-                        Cargar
-                        <v-icon class="ml-2">mdi-file-upload</v-icon>
-                      </v-btn>
-                    </template>
-                  </v-file-input>
-                </ValidationProvider>
+                        <template v-slot:append-outer>
+                          <v-btn
+                              v-if="file && month"
+                              style="bottom: 8px !important;"
+                              color="primary"
+                              dark
+                              @click="fileUpload"
+                          >
+                            Cargar
+                            <v-icon class="ml-2">mdi-file-upload</v-icon>
+                          </v-btn>
+                        </template>
+                      </v-file-input>
+                    </ValidationProvider>
+                  </v-col>
+                </v-row>
               </ValidationObserver>
             </v-col>
             <v-col cols="12" v-if="errors.length">
-              <v-subheader class="title error--text">El archivo cargado presenta {{ errors.length }} error{{errors.length === 1 ? '' : 'es'}}</v-subheader>
+              <v-subheader class="title error--text">El archivo cargado presenta {{ errors.length }}
+                error{{ errors.length === 1 ? '' : 'es' }}
+              </v-subheader>
               <v-simple-table>
                 <template v-slot:default>
                   <tbody>
@@ -124,10 +144,12 @@
 
 <script>
 import {store} from '@/store/store'
+
 export default {
   name: 'ExcelFileUploader',
   data: () => ({
     file: null,
+    month: null,
     errors: [],
     dialog: false,
     loading: false,
@@ -135,43 +157,47 @@ export default {
   }),
   watch: {
     dialog: {
-      handler (val) {
+      handler(val) {
         if (val) this.open()
       },
       immediate: false
     }
   },
   methods: {
-    open () {
+    open() {
       this.dialog = true
     },
-    close () {
+    close() {
       this.$refs.formFile.reset()
       this.file = null
+      this.month = null
       this.errors = []
       this.dialog = false
     },
-    fileUpload () {
+    fileUpload() {
       this.$refs.formFile.validate().then(result => {
         if (result) {
           this.loading = true
           this.errors = []
           let data = new FormData()
           data.append('archivo', this.file)
+          data.append('mes', this.month)
           this.axios.post('load-seguimientos', data)
               .then(() => {
                 this.loading = false
-                this.$store.commit('snackbar', {color: 'success', message: 'El cargue del archivo se realizó correctamente.'})
+                this.$store.commit('snackbar', {
+                  color: 'success',
+                  message: 'El cargue del archivo se realizó correctamente.'
+                })
                 store.commit('atencionMedicaRCVModule/SET_COUNTER')
                 this.close()
               })
               .catch(error => {
                 this.loading = false
-                if (error?.response?.data?.errors?.length)  {
+                if (error?.response?.data?.errors?.length) {
                   this.errors = error.response.data.errors
                   store.commit('atencionMedicaRCVModule/SET_LOADS_COUNTER')
-                }
-                else {
+                } else {
                   this.$store.commit('snackbar', {color: 'error', message: ' al cargar el archivo.', error: error})
                 }
               })
